4f979da164c11d53b1b1fdb73e338ad7ebcad8cd
Paulinho <pmagno15@hotmail.com>
2023-01-03 17:22:10 -0300

    app final

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import pickle
-#import sys
-#sys.path.append("/opt/anaconda3/lib/python3.9/site-packages")
-
 import streamlit as st
 import pandas as pd
 import requests 
+from PIL import Image
+import base64
 
+# função que retorna a imagem do filme do tmdb oelo id
 def get_poster(movie_id):
 
     API_KEY = 'd2dee50252ec42a906562d4f3c43dfb0'
@@ -17,7 +17,7 @@ def get_poster(movie_id):
     path = "https://image.tmdb.org/t/p/w500" + poster_path
     return path 
      
-
+# função que retorna os ids e url do poster das 5 recomendações
 def rec_function(movie):
 
     idx = movies_data[movies_data['title'] == movie].index[0]
@@ -36,16 +36,98 @@ def rec_function(movie):
 
     return name_movies_rec, poster_movies_rec
 
+# configuraçã da pagina
+st.set_page_config( page_title = 'Recommender system',
+    page_icon = 'https://www.svgrepo.com/show/34896/movie.svg',
+     layout = 'centered',
+    initial_sidebar_state = 'expanded'
+)
 
-st.header('Sistema de recomendação de filmes')
+st.header(':white[Sistema de recomendação de filmes]')
 
+
+# carregamento da lista de filmes e as similaridades
 dict_movies = pickle.load(open("C:/Users/prpau/Downloads/Recomendacao/movies_list.pkl", 'rb'))
 similarity = pickle.load(open("C:/Users/prpau/Downloads/Recomendacao/similarity.pkl", 'rb'))
 movies_data = pd.DataFrame(dict_movies)
 
 titles = list(movies_data['title'])
-select_movie = st.selectbox("Selecionar filme", titles)
+select_movie = st.sidebar.selectbox("**SELECIONAR FILME**", titles)
+id = movies_data[movies_data['title'] == select_movie]['id'].values[0]
 
+# mostra a imagem do poster do filme selecionado
+image = get_poster(id)
+st.sidebar.image(image)
+
+
+# background da pagina
+
+
+def add_bg_from_local(image_file):
+
+    img = Image.open(image_file)
+    img = img.copy()
+    img.putalpha(50)
+    img.save('background.png')
+
+    with open('background.png', "rb") as image2:
+
+       
+        encoded_string = base64.b64encode(image2.read())
+
+        
+
+    st.markdown(
+    f"""
+    <style>
+    .stApp {{
+        background-image: url(data:image/{"png"};base64,{encoded_string.decode()});
+        
+        background-position: right;
+        background-attachment: fixed;
+    }}
+    </style>
+    """,
+    unsafe_allow_html=True
+    )
+
+name = 'images/background.jpg' 
+add_bg_from_local(name)  
+
+
+
+#def convertImage():
+ #   img = Image.open(name)
+  #  img = img.copy()
+   # img.putalpha(180)
+    #img.save('transparent.png')
+    #return img
+
+#icon = convertImage()
+
+
+#def add_bg_from_url():
+
+ #   st.markdown(
+  #       f"""
+   #      <style>
+    #     .stApp {{
+     #        background-image: url(data:image/{"png"};base64,{encoded_string.decode()}));
+      #       background-size: 20%;
+       #      background-repeat: no-repeat;
+        #     background-position: right;
+         #    background-attachment: fixed;
+         #    background-color: str
+             
+         #}}
+         #</style>
+         #""",
+         #unsafe_allow_html=True
+     #)
+
+#add_bg_from_url() 
+
+# botão que retorna as recomendações com imagemdo poster de cada
 if st.button("Mostrar recomendações"):
 
     name_movies_rec, poster_movies_rec = rec_function(select_movie)
